46d5ac2b0e3da5798d73ca3a6da714ff29eb4178
dadaa <dadaa@users.noreply.github.com>
2020-05-17 17:18:44 +0900

    Updates

---
+++ precache-manifest.bcdca36b2cdabb8e11fecdb22ca625d7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e549e3c98f46a9ba44baca9bfa3a98",
+    "url": "/cocosoco/index.html"
+  },
+  {
+    "revision": "f4276aa916783afb78e7",
+    "url": "/cocosoco/static/css/main.19be8e55.chunk.css"
+  },
+  {
+    "revision": "692e44fc01097f3d5459",
+    "url": "/cocosoco/static/js/2.336b8ded.chunk.js"
+  },
+  {
+    "revision": "d628b86b05225b4ee57b19ea2088d87a",
+    "url": "/cocosoco/static/js/2.336b8ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4276aa916783afb78e7",
+    "url": "/cocosoco/static/js/main.60dfcb61.chunk.js"
+  },
+  {
+    "revision": "f97a5b8249d84b244e04",
+    "url": "/cocosoco/static/js/runtime-main.c896fc62.js"
+  },
+  {
+    "revision": "728874bb0fb539dd68b40b81e84fd691",
+    "url": "/cocosoco/static/media/AudioMuting.728874bb.svg"
+  },
+  {
+    "revision": "288ed0b31bad59700b817a5d7fae3249",
+    "url": "/cocosoco/static/media/AudioMutingDisabled.288ed0b3.svg"
+  },
+  {
+    "revision": "6dfcbc6bc6f9713b04c6a5249b1df420",
+    "url": "/cocosoco/static/media/CameraMuting.6dfcbc6b.svg"
+  },
+  {
+    "revision": "cd67b22a3e8f64661ba749a53e29a569",
+    "url": "/cocosoco/static/media/CameraMutingDisabled.cd67b22a.svg"
+  },
+  {
+    "revision": "e1a723d2b40adf5b53555a15c2143693",
+    "url": "/cocosoco/static/media/CameraSwitching.e1a723d2.svg"
+  },
+  {
+    "revision": "4cb62ad4f7dcdb7ba50596fe780223fe",
+    "url": "/cocosoco/static/media/Pointing.4cb62ad4.png"
+  }
+]);
