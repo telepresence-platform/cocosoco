2424b22ebc9e438c6b20fb40fe3596c635a759d8
sample <niufsd8@gmail.com>
2020-10-28 18:46:44 +0900

    OK

--- src/components/MapPanel.tsx
+++ src/components/MapPanel.tsx
@@ -1,26 +1,40 @@
 import React from "react";
+import GoogleMapReact from 'google-map-react';
 import { connect } from "react-redux";
 import { ThunkDispatch } from "redux-thunk";
 import { AnyAction } from "redux";
 
 import { TStore } from "../store";
 import { toggleMapMuting } from "../actions"
+import { InitializeMap } from "../actions"
 
 import "./MapPanel.css";
 
 interface IProps {
+  mapkey: string,
+  lat: number,
+  lng: number,
+  defaultZoom: number,
   isMapEnabled: boolean,
-  toggleMapMuting: any,
 }
 
 class MapPanel extends React.PureComponent<IProps> {
   render() {
-    const { isMapEnabled } = this.props;
+    const { isMapEnabled, mapkey, lat, lng, defaultZoom } = this.props;
     const className = isMapEnabled ? "mappanel" : "mappanel--disabled";
 
     return (
       <div className={className}>
-        MAP
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: mapkey
+          }}
+          defaultCenter={{
+            lat: lat,
+            lng: lng
+          }}
+          defaultZoom={defaultZoom}>
+        </GoogleMapReact>
       </div>
     );
   }
@@ -29,10 +43,17 @@ class MapPanel extends React.PureComponent<IProps> {
 const mapStateToProps = (store: TStore) => {
   return {
     isMapEnabled: store.state.isMapEnabled,
+    mapkey: store.state.mapkey,
+    lat: store.state.lat,
+    lng: store.state.lng,
+    defaultZoom: store.state.defaultZoom,
   }
 }
 
 const mapDispatchToProps = (dispatch: ThunkDispatch<TStore, void, AnyAction>) => ({
+  InitializeMap: (mapkey: string, lat: number, lng: number, defaultZoom: number) => {
+    dispatch(InitializeMap(mapkey,lat, lng, defaultZoom));
+  },
   toggleMapMuting: () => {
     dispatch(toggleMapMuting());
   },
