ed0087f47418d3702b4745058947b03ae3651d78
yogurt0713 <yogurt0713@gmail.com>
2020-11-14 21:07:26 +0900

    fixed #13

--- src/lib/video.tsx
+++ src/lib/video.tsx
@@ -1,23 +1,23 @@
 let currentIndex = -1;
-
+let currentStream: any = null;
 async function getDevices() {
   const devices = await navigator.mediaDevices.enumerateDevices();
   return devices.filter(device => device.kind === "videoinput");
 }
-
 export async function nextVideoStream() {
+  if (currentStream) {
+    currentStream.getTracks().forEach((track: any) => {
+      track.stop();
+    });
+  }
   const devices = await getDevices();
-
   const nextIndex = currentIndex === devices.length - 1 ? 0 : currentIndex + 1;
   const nextDevice = devices[nextIndex];
-
-  const deviceId = nextDevice.deviceId;
-  const stream = await navigator.mediaDevices.getUserMedia({
+  const deviceId = { exact: nextDevice.deviceId };
+  currentIndex = nextIndex;
+  currentStream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: { deviceId },
   });
-
-  currentIndex = nextIndex;
-
-  return stream;
+  return currentStream;
 }
