fbfdbcf403c298ea2972ad09ad69e102cc15b08a
sample <niufsd8@gmail.com>
2021-01-21 19:47:39 +0900

    create demo

--- src/components/AudienceItem.tsx
+++ src/components/AudienceItem.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { connect } from "react-redux";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
 
+import { TStore } from "../store";
+import { selectPeer } from "../actions";
 import { Member } from "../types";
 
 import "./AudienceItem.css";
@@ -8,11 +13,23 @@ interface IProps {
   audience: Member;
   isMuted: boolean;
   isSelected: boolean;
+  selectPeer: any;
 }
 
 class AudienceItem extends React.PureComponent<IProps> {
   // As video.playsInline is not defined in HTMLVideoElement, add "any" as well.
   private _videoRef = React.createRef<HTMLVideoElement | any>();
+
+  constructor(props: IProps) {
+    super(props);
+
+    this._onClick = this._onClick.bind(this);
+  }
+
+  _onClick() {
+    const { audience, selectPeer } = this.props;
+    selectPeer(audience.peerId);
+  }
 
   _updateVideo() {
     const video = this._videoRef.current;
@@ -39,10 +56,19 @@ class AudienceItem extends React.PureComponent<IProps> {
     return (
       <li
         className={
-          "audience-item " + (isSelected ? "audience-item--selected" : "")
+          "audience-item" +
+          (isSelected ? " audience-item--selected" : "") +
+          (isMuted ? " audience-item--mine" : "")
         }
+        onClick={this._onClick}
       >
-        <img src={audience.dataURL} className="audience-item__icon"></img>
+        <img
+          src={audience.dataURL}
+          className={
+            "audience-item__icon" +
+            (isMuted ? " audience-item__icon--mine" : "")
+          }
+        ></img>
         <video
           className="audience-item__video"
           muted={isMuted}
@@ -53,4 +79,12 @@ class AudienceItem extends React.PureComponent<IProps> {
   }
 }
 
-export default AudienceItem;
+const mapDispatchToProps = (
+  dispatch: ThunkDispatch<TStore, void, AnyAction>
+) => ({
+  selectPeer: (peerId: string) => {
+    dispatch(selectPeer(peerId));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(AudienceItem);
